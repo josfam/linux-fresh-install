e358b4967bf6ee4c0163818b0df61157c38ef6ce
josfam <josephamukun@tutanota.com>
2024-04-05 10:51:14 +0300

    Add development script, & remove `building....sh`

--- building-things.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Install tools for building and making, generally
-sudo apt install -y \
-	build-essential \
-	cmake
---
+++ development.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# For software-development/coding tools
+
+sudo apt update
+
+# General
+sudo apt install -y \
+	build-essential \
+	cmake \
+	bash \
+	curl \
+	rlwrap
+
+# Adds dependencies that are usually required when building Python from source
+sudo apt install -y \
+		 pkg-config \
+		 python3-tk \
+		 libsqlite3-dev \
+		 libncurses5-dev \
+		 libgdbm-dev \
+		 libbz2-dev \
+		 libdb-dev \
+		 uuid-dev \
+		 libffi-dev \
+		 liblzma-dev \
+		 libreadline-dev \
+		 libssl-dev \
+		 tk-dev \
+		 zlib1g-dev
+
+# Valgrind
+sudo apt install valgrind
+
+# Java
+sudo apt install -y default-jdk
+
+# HomeBrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Clojure (language)
