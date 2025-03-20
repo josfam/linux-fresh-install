ed7c6acc1be1a0398f610cd93b1a4999f1de5e8c
josfam <jamukun@proton.me>
2025-03-20 23:28:37 +0300

    Update librewolf installation

--- web-browsers.sh
+++ web-browsers.sh
@@ -21,20 +21,6 @@ sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
 
 ## Librewolf
-sudo apt update && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
-
-distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
-
-wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-
-sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
-Types: deb
-URIs: https://deb.librewolf.net
-Suites: $distro
-Components: main
-Architectures: amd64
-Signed-By: /usr/share/keyrings/librewolf.gpg
-EOF
-
-sudo apt update
-sudo apt install librewolf -y
+sudo apt update && sudo apt install extrepo -y
+sudo extrepo enable librewolf
+sudo apt update && sudo apt install librewolf -y
