e23841674a5a762b86e25aeba5a0c737d17480b7
josfam <jamukun@proton.me>
2025-03-24 16:55:18 +0300

    Add gogh color scheme information

--- zsh-install.sh
+++ zsh-install.sh
@@ -3,3 +3,14 @@
 # install zsh and oh-my-sh
 sudo apt install -y zsh \
 && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Gogh colors
+#
+# Everblush (122)
+# Everforest Dark Hard (123)
+# Github Dark (143)
+#
+# Gruvbox (155)
+# Solarized Light (288)
+# Tokyo Night Light (327)
+# Novel (233)
