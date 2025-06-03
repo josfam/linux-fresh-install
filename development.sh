cfa74de39bbf05202f2736da2791b5472c2acb0b
josfam <jamukun@proton.me>
2025-06-03 12:39:45 +0300

    Add tmux installation and config creation

--- development.sh
+++ development.sh
@@ -58,3 +58,23 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 
 # postgresql installation
 sudo apt install postgresql
+
+# tmux installation
+sudo apt install -y tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# create tmux configuration file and populate it with basic settings
+touch ~/.tmux.conf
+echo """# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+
+# Other examples:
+
+# set -g @plugin 'github_username/plugin_name'
+# set -g @plugin 'github_username/plugin_name#branch'
+# set -g @plugin 'git@github.com:user/plugin'
+# set -g @plugin 'git@bitbucket.com:user/plugin'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'""" > ~/.tmux.conf
