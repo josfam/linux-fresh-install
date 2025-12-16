9be73fdde1f0dfa6ec2af8c9c78cdbd477d51dd0
josfam <jamukun@proton.me>
2025-12-16 10:41:32 +0300

    Update comment for home-directories.sh

--- home-directories.sh
+++ home-directories.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# creates other standard repos in the home directory
+# creates other standard directories that I use in the home directory
 cd ~
 mkdir my-repos programs my-scripts other-repos
